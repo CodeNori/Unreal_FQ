eb302702a6f1ebfc2e54ea05b05fe5fb8ee4d842
CodeNori <83115461+CodeNori@users.noreply.github.com>
2024-04-18 21:09:50 +0900

    처음 업로드

---
+++ Source/U4CpGame/U4CpGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "U4CpGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U4CPGAME_API AU4CpGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
